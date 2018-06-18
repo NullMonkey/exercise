a278047a9e08e352c4b97a4976cb7a795dd87194
NullMonkey <ghostdrifter@gmail.com>
2018-06-18 18:26:57 -0400

    added some things
    player clas implements JsonSerializable
    experments with SplObjectStorage in player collections

--- index.php
+++ index.php
@@ -8,12 +8,25 @@ require_once __DIR__ . '/autoload.php';
 
 $newPlayer = new  \excercise\Models\Player('bob', '22', 'waterBoy', '0');
 
-echo serialize($newPlayer);
+$json = json_encode($newPlayer);
+echo $json;
+
+//echo serialize($newPlayer);
 
 $newCollect = new excercise\Models\PlayersCollection();
 
 echo '<br />';
 
+$newCollect->attach($newPlayer);
 
+//echo '<br />'.print_r($newCollect);
 
+//echo $newCollect->serialize()."\n";
 
+foreach ($newCollect as $value) {
+    $obj = $newCollect->current(); // current object
+    $assoc_key  = $newCollect->getInfo(); // return, if exists, associated with cur. obj. data; else NULL
+    $json = json_encode($obj);
+    echo $json;
+
+}
