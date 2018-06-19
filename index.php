25504e6e2e78125a9f4c85ec95e541e75e285ffe
NullMonkey <ghostdrifter@gmail.com>
2018-06-19 05:08:55 -0400

    added DisplayVM chooses the View based on value in PHP_SAPI Interfaces are not used at this time

--- index.php
+++ index.php
@@ -2,16 +2,12 @@
 
 require_once __DIR__ . '/autoload.php';
 
-/*
-$model = new \excercise\Model\Model();
-
-
-$controller = new \excercise\Controller\Controller($model);
-$view = new \excercise\View\View($controller, $model);
-echo $view->output();
-*/
 
 $newPlayer = new  \excercise\Model\Player('bob', '22', 'waterBoy', '0');
 
-$json = json_encode($newPlayer);
-echo $json;
+$team = new \excercise\ViewModel\PlayersVM();
+$team->attach($newPlayer);
+
+
+
+$playersView = new \excercise\ViewModel\DisplayVM($team);
